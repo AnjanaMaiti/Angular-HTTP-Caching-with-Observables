1d1a7e6e0ff9e520d4f56b251a36f80cb7096e7a
AnjanaMaiti <anjanamaiti15@gmail.com>
2020-12-17 00:41:07 +0530

    http caching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map,startWith } from 'rxjs/operators';
+
+const CACHE_KEY = "httpRepoCache";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HTTP-caching';
+  repos:any;
+  constructor(http: HttpClient){
+    const path = 'http://api.github.com/search/repositories?q=angular';
+    this.repos = http.get<any>(path)
+    .pipe(
+      map(data => data.items)
+    )
+
+    //subscripe to "this.repos" observable and save it in localStorage
+    this.repos.subscribe(next => {
+        localStorage[CACHE_KEY] = JSON.stringify(next);
+    });
+
+    //startWith  takes in an Observable/string of events and add those events in the very beginning.Basically it will synchronously emit 
+    this.repos = this.repos.pipe(
+      startWith(JSON.parse(localStorage[CACHE_KEY] || '[]'))
+    )
+  }
 }
